55729ae082337dbbf8f90d59cd88fa0ec8c6060e
luizamaciel <luiza.maciel@b2wdigital.com>
2023-03-01 19:43:06 -0300

    remove mockedTime não usado do teste de newcurrency

--- useCases/newCurrency_test.go
+++ useCases/newCurrency_test.go
@@ -6,11 +6,9 @@ import (
 	"github.com/lumacielz/challenge-bravo/database"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"time"
 )
 
 func TestCurrencyUseCase_NewCurrency(t *testing.T) {
-	mockedTime := time.Date(2023, 03, 1, 18, 31, 10, 0, time.UTC)
 	type args struct {
 		currencyRepositoryError error
 		currency                CurrencyRequest
@@ -47,7 +45,6 @@ func TestCurrencyUseCase_NewCurrency(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := CurrencyUseCase{
-				Now:                func() time.Time { return mockedTime },
 				CurrencyRepository: database.Mock{Error: tt.args.currencyRepositoryError},
 			}
 			got, err := c.NewCurrency(context.Background(), tt.args.currency)
