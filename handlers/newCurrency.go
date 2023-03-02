7901b1fed85d4795fe1fee564ecba76e7661e33f
luizamaciel <luiza.maciel@b2wdigital.com>
2023-03-02 00:48:06 -0300

    cria testes para newCurrency

--- handlers/newCurrency.go
+++ handlers/newCurrency.go
@@ -1,20 +1,56 @@
 package handlers
 
 import (
+	"context"
+	"github.com/lumacielz/challenge-bravo/entities"
+	"github.com/lumacielz/challenge-bravo/useCases"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func (c CurrencyController) NewCurrencyHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	ctx, cancel := context.WithTimeout(r.Context(), 500*time.Millisecond)
+	defer cancel()
 
-	body, err := ioutil.ReadAll(r.Body)
-	currencyReq, err := c.InputPresenter.ParseRequest(body)
-	insertedId, err := c.UseCase.NewCurrency(ctx, currencyReq)
-	if err != nil {
-		c.OutputPresenter.WriteError(w, err, 500)
+	respC := make(chan useCases.NewCurrencyResponse)
+	errC := make(chan error, 1)
+	go func() {
+		defer close(errC)
+		defer close(respC)
+
+		body, _ := ioutil.ReadAll(r.Body)
+		currencyReq, err := c.InputPresenter.ParseRequest(body)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		resp, err := c.UseCase.NewCurrency(ctx, currencyReq)
+		if err != nil {
+			errC <- err
+			return
+		}
+		respC <- resp
+	}()
+
+	var status int
+	select {
+	case <-ctx.Done():
+		c.OutputPresenter.WriteError(w, useCases.ErrRequestTimeout, http.StatusRequestTimeout)
 		return
+	case resp := <-respC:
+		c.OutputPresenter.WriteResponse(w, &resp, http.StatusCreated)
+		return
+	case err := <-errC:
+		switch err {
+		case entities.ErrCodeRequired:
+			status = http.StatusBadRequest
+		case entities.ErrZeroConversionRate:
+			status = http.StatusUnprocessableEntity
+		default:
+			status = http.StatusInternalServerError
+		}
+		c.OutputPresenter.WriteError(w, err, status)
 	}
-
-	c.OutputPresenter.WriteResponse(w, insertedId, http.StatusCreated)
 }
