06ab488f34ba462aeefc816a6a0eb9790423e532
luizamaciel <luiza.maciel@b2wdigital.com>
2023-03-02 09:49:08 -0300

    cria teste de deleteCurrencyHandler

--- handlers/deleteCurrency.go
+++ handlers/deleteCurrency.go
@@ -1,19 +1,43 @@
 package handlers
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5"
+	"github.com/lumacielz/challenge-bravo/entities"
+	"github.com/lumacielz/challenge-bravo/useCases"
 	"net/http"
+	"time"
 )
 
 func (c CurrencyController) DeleteCurrencyHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	code := chi.URLParam(r, "code")
+	ctx, cancel := context.WithTimeout(r.Context(), 500*time.Millisecond)
+	defer cancel()
 
-	err := c.UseCase.CurrencyRepository.Delete(ctx, code)
-	if err != nil {
-		c.OutputPresenter.WriteError(w, err, http.StatusInternalServerError)
+	errC := make(chan error, 1)
+	go func() {
+		defer close(errC)
+
+		code := chi.URLParam(r, "code")
+
+		err := c.UseCase.DeleteCurrency(ctx, code)
+		errC <- err
+	}()
+
+	var status int
+	select {
+	case <-ctx.Done():
+		c.OutputPresenter.WriteError(w, useCases.ErrRequestTimeout, http.StatusRequestTimeout)
 		return
+	case err := <-errC:
+		switch err {
+		case nil:
+			c.OutputPresenter.WriteResponse(w, nil, http.StatusNoContent)
+			return
+		case entities.ErrCurrencyNotFound:
+			status = http.StatusNotFound
+		default:
+			status = http.StatusInternalServerError
+		}
+		c.OutputPresenter.WriteError(w, err, status)
 	}
-
-	c.OutputPresenter.WriteResponse(w, nil, http.StatusNoContent)
 }
---
+++ handlers/deleteCurrency_test.go
@@ -0,0 +1,99 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/lumacielz/challenge-bravo/database"
+	"github.com/lumacielz/challenge-bravo/external"
+	"github.com/lumacielz/challenge-bravo/presenters"
+	"github.com/lumacielz/challenge-bravo/useCases"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestCurrencyController_DeleteCurrencyHandler(t *testing.T) {
+	cancelledCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+	type args struct {
+		ctx             context.Context
+		code            string
+		repositoryError error
+		wantStatus      int
+		wantPath        string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx:        context.Background(),
+				code:       "BRL",
+				wantStatus: http.StatusNoContent,
+				wantPath:   "/responses/empty.json",
+			},
+		},
+		{
+			name: "timeout",
+			args: args{
+				ctx:        cancelledCtx,
+				wantStatus: http.StatusRequestTimeout,
+				wantPath:   "/responses/errTimeout.json",
+			},
+		},
+		{
+			name: "currency not found",
+			args: args{
+				ctx:        context.Background(),
+				code:       "BL",
+				wantStatus: http.StatusNotFound,
+				wantPath:   "/responses/errNotFound.json",
+			},
+		},
+		{
+			name: "internal server error",
+			args: args{
+				ctx:             context.Background(),
+				code:            "BRL",
+				repositoryError: errors.New("unexpected error"),
+				wantStatus:      http.StatusInternalServerError,
+				wantPath:        "/responses/errInternalServer.json",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := CurrencyController{
+				UseCase: useCases.CurrencyUseCase{
+					Now:                 func() time.Time { return time.Now() },
+					CurrencyRepository:  database.Mock{Error: tt.args.repositoryError},
+					QuotationRepository: external.QuotationMock{},
+				},
+				OutputPresenter: presenters.JsonPresenter{},
+				InputPresenter:  presenters.JsonPresenter{},
+			}
+
+			w := httptest.NewRecorder()
+
+			r := httptest.NewRequest("DELETE", "/", nil)
+			rctx := chi.NewRouteContext()
+			rctx.URLParams.Add("code", tt.args.code)
+			r = r.WithContext(context.WithValue(tt.args.ctx, chi.RouteCtxKey, rctx))
+
+			c.DeleteCurrencyHandler(w, r)
+
+			root, _ := os.Getwd()
+			respBody, _ := ioutil.ReadAll(w.Body)
+			wantBody, _ := ioutil.ReadFile(root + tt.args.wantPath)
+			assert.Equal(t, string(wantBody), string(respBody))
+			assert.Equal(t, tt.args.wantStatus, w.Code)
+		})
+	}
+}
